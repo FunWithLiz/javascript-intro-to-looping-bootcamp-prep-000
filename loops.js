93195e7d266864a1dec5a48d239b833a69ead5b1
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-24 18:50:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,64 +1,32 @@
 function forLoop(array){
   for (let i = 0; i<25; i++) {
-    if (i<1) {
-      return (`I am 1 strange loop`)
-    }
-  else if(i>2){
-    return (`I am ${i} strange loops`)
-    return array
-  }
-  }
-}
-
-
-
-function whileLoop(n){
-  return Math.random()>0
-}
-
-while (whileLoop(n)){
-  return('done')
-}
-
-
-function doWhileLoop(array){
-return Math.random()
-}
-
-do{
-  return('done');
-} while (array.length > 0 && doWhileLoop());
-
-
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+    if (i===1) {
       array.push("I am 1 strange loop.")
     } else {
       array.push(`I am ${i} strange loops.`)
     }
   }
-
   return array
 }
+
 
 
 function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
   }
-
   return 'done'
 }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
+
+
+function maybeTrue() {
     return Math.random() >= 0.5
-  }
-
+}
+  
+function doWhileLoop(array) {
   do {
-    array = array.slice(1)
+    array = array.shift(1)
   } while (array.length > 0 && maybeTrue())
-
   return array
 }
