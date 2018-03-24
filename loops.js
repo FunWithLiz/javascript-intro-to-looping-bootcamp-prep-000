aacd770a6d9f37ce908c86180e4e605a90b362d9
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-24 18:48:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -28,3 +28,37 @@ return Math.random()
 do{
   return('done');
 } while (array.length > 0 && doWhileLoop());
+
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+
+  return array
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
